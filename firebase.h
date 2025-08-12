e8b692d952f47471c1a0bccac544c2ef5b072ccb
Bùi Quang Tấn <123096225+buiTannn@users.noreply.github.com>
2025-08-12 22:16:21 +0700

    Update firebase.h

--- firebase.h
+++ firebase.h
@@ -6,6 +6,7 @@ void firebase_put(const char* path, const char* data);
 void firebase_get(const char* path, char* response_buf, int buf_size);
 void firebase_patch(const char* path, const char* data);
 void firebase_delete(const char* path);
+void firebase_cleanup();
 bool firebase_test_connection();
 
 #endif
