86b0f11d5303a8b534a6c15491811f2552b03447
Bùi Quang Tấn <123096225+buiTannn@users.noreply.github.com>
2025-08-28 16:11:35 +0700

    fix(fb.h) delete test connection function

--- firebase.h
+++ firebase.h
@@ -7,6 +7,5 @@ void firebase_get(const char* path, char* response_buf, int buf_size);
 void firebase_patch(const char* path, const char* data);
 void firebase_delete(const char* path);
 void firebase_cleanup();
-bool firebase_test_connection();
 
 #endif
